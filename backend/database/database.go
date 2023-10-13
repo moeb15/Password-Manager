13a2da02c3741df4c58db8993122e65f8b3e9749
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-13 19:36:25 -0400

    reformatted

--- backend/database/database.go
+++ backend/database/database.go
@@ -3,7 +3,13 @@ package database
 import (
 	"context"
 	"log"
+	"os"
+	"pwdmanager_api/helpers"
+	"pwdmanager_api/models"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,4 +30,21 @@ func Connect(dbURL string) *DB {
 	}
 
 	return &DB{client: client}
+}
+
+func (db *DB) CreateUser(user models.User) *models.User {
+	user_coll := db.client.Database(os.Getenv("DB_NAME")).Collection("users")
+	ctx, cancel := context.WithTimeout(context.TODO(), 30*time.Second)
+	defer cancel()
+	insert, err := user_coll.InsertOne(ctx, bson.D{{Key: "name", Value: user.Username},
+		{Key: "password", Value: helpers.HashPwd(user.Password)},
+		{Key: "masterkey", Value: helpers.HashPwd(user.MasterKey)}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	insert_id := insert.InsertedID.(primitive.ObjectID).Hex()
+	returned_user := models.User{ID: insert_id, Username: user.Username}
+	return &returned_user
 }
