159e674c5078062fb853a23b789427d4751d759e
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-13 19:38:58 -0400

    added resource for creating users

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
-	"pwdmanager_api/helpers"
+	"pwdmanager_api/controller"
+	"pwdmanager_api/database"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	var pwd string
-	fmt.Scanln(&pwd)
-	fmt.Println(helpers.HashPwd(pwd))
+	loadEnv()
+	db := database.Connect(os.Getenv("DB_URL"))
+	serveApplication(db)
 }
 
-func Listen() {
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load environment variables")
+	}
+}
+
+func serveApplication(db *database.DB) {
 	router := gin.Default()
-	log.Fatal(router.Run(":8000"))
+
+	public_routes := router.Group("/auth")
+	public_routes.POST("/register", controller.Register(db))
+
+	log.Fatal(router.Run(":8080"))
 }
