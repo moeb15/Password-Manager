6831044ea9989fca1afc4bec982d1f1929818e6c
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-15 15:59:04 -0400

    added handler for user login

--- backend/internal/controller/users.go
+++ backend/internal/controller/users.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"net/http"
+	"pwdmanager_api/internal/auth"
 	"pwdmanager_api/internal/database"
+	"pwdmanager_api/internal/helpers"
 	"pwdmanager_api/pkg/models"
 
 	"github.com/gin-gonic/gin"
@@ -10,12 +12,39 @@ import (
 
 func Register(c *gin.Context) {
 	db := c.MustGet("db_conn").(*database.DB)
-	var user models.User
-	if err := c.ShouldBindJSON(&user); err != nil {
+	var user_auth models.AuthInput
+	if err := c.ShouldBindJSON(&user_auth); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user := models.User{
+		Username: user_auth.Username,
+		Password: user_auth.Password,
+	}
+	created_user := db.CreateUser(user)
+	c.JSON(http.StatusCreated, gin.H{"data": created_user})
+}
+
+func Login(c *gin.Context) {
+	db := c.MustGet("db_conn").(*database.DB)
+	var user_auth models.AuthInput
+	if err := c.ShouldBindJSON(&user_auth); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	created_user := db.CreateUser(user)
-	c.JSON(http.StatusCreated, gin.H{"data": created_user})
+	user, err := db.FindUserByName(user_auth.Username)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	if ok := helpers.CompareHashes(user_auth.Password, user.Password); !ok {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	jwt, err := auth.GenerateJWT(*user)
+
+	c.JSON(http.StatusOK, gin.H{"jwt": jwt})
 }
