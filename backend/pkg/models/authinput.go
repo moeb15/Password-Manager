bb817cab5635ffff6c0781521c74eabd8f9dbbee
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-16 16:21:29 -0400

    added model for passwords

--- backend/pkg/models/authinput.go
+++ backend/pkg/models/authinput.go
@@ -4,3 +4,9 @@ type AuthInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
+type AuthPwd struct {
+	Application string `json:"application" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	Key         string `json:"key" binding:"required"`
+}
