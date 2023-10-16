1e236b6022d13fc7ad17dde0fdfd44d838090ae2
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-16 16:22:45 -0400

    contains handlers for password related requests

---
+++ backend/internal/controller/passwords.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"log"
+	"net/http"
+	"pwdmanager_api/internal/auth"
+	"pwdmanager_api/internal/database"
+	"pwdmanager_api/internal/helpers"
+	"pwdmanager_api/pkg/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddPassword(c *gin.Context) {
+	db := c.MustGet("db_conn").(*database.DB)
+	var pwd models.AuthPwd
+	if err := c.ShouldBindJSON(&pwd); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := auth.CurrentUser(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if ok := helpers.CompareHashes(pwd.Key, user.MasterKey); !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid key"})
+		return
+	}
+
+	enc_pwd, err := helpers.EncryptAES([]byte(pwd.Key), pwd.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	saved_pwd := models.Password{
+		UserID:      user.ID,
+		Application: pwd.Application,
+		Password:    enc_pwd,
+	}
+	user.SavedPwds = append(user.SavedPwds, saved_pwd)
+	db.CreatePassword(saved_pwd, user)
+	c.JSON(http.StatusCreated, gin.H{"data": ""})
+}
