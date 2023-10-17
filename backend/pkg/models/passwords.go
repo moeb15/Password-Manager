7390e974ce68a21168b90a5073d13eeb900e7b51
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-17 17:47:03 -0400

    updated passwords model

--- backend/pkg/models/passwords.go
+++ backend/pkg/models/passwords.go
@@ -2,7 +2,7 @@ package models
 
 type Password struct {
 	ID          string `json:"id" bson:"_id"`
-	UserID      string `json:"userid"`
-	Application string `json:"application"`
-	Password    string `json:"password"`
+	UserID      string `json:"userid" bson:"userid"`
+	Application string `json:"application" bson:"application"`
+	Password    string `json:"password" bson:"password"`
 }
