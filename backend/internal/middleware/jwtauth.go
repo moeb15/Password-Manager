337d4c461b7584c787b1ca16eca9710c13b84943
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-19 20:33:48 -0400

    jwt middleware now checks if a valid refresh token was provided in the request, if so a new access token is generated

--- backend/internal/middleware/jwtauth.go
+++ backend/internal/middleware/jwtauth.go
@@ -11,9 +11,26 @@ func JWTAuthMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		err := auth.ValidateJWT(c)
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-			c.Abort()
-			return
+			err = auth.ValidateRefreshToken(c)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			rf_token, err := auth.GetRefreshToken(c)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			access_token, err := auth.JWTFromRefresh(rf_token)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+				c.Abort()
+				return
+			}
+			c.JSON(http.StatusCreated, gin.H{"access_token": access_token})
+			c.Request.Header.Set("Authroization", access_token)
 		}
 		c.Next()
 	}
