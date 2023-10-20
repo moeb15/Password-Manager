0c813de059c51bd0f6dc4c4d4d92954cc5b3e175
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-20 02:25:11 -0400

    removed savedpwds field from users model

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -1,9 +1,8 @@
 package models
 
 type User struct {
-	ID        string     `json:"id" bson:"_id"`
-	Username  string     `json:"username"`
-	Password  string     `json:"password"`
-	MasterKey string     `json:"masterkey"`
-	SavedPwds []Password `json:"savedpasswords"`
+	ID        string `json:"id" bson:"_id"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	MasterKey string `json:"masterkey"`
 }
