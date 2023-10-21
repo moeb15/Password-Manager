55de09e1a843f9df14891b2e1e57a2479ce4814d
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-21 19:01:22 -0400

    login component initial commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,9 @@
+function Login(){
+    return(
+        <div className="w-full h-screen text-center items-center
+                        text-gray-300">
+        </div>
+    )
+}
+
+export default Login;
