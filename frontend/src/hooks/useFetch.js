143f77521cefc17bcf32d5b67ae12831448b8a79
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-21 18:47:01 -0400

    custom hook for fetching and setting data

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useEffect,useState } from "react";
+
+
+const useFetch = (url,{props}) => {
+    const [ data,setData ] = useState(null);
+    const [ err,setError ] = useState(null);
+
+    useEffect(()=>{
+        const getData = async() => {
+            try{
+                const response = await fetch(url,{
+                    method: props.method,
+                    headers: props.headers,
+                    body: props.body
+                })
+                const response_json = await response.json()
+                setData(response_json)
+            }catch (error){
+                setError(error)
+            }
+        }
+    },[url, props])
+}
