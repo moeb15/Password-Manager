ed2f29598d74d82443f56ebcab88280e50cd3d3c
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-23 04:53:38 -0400

    created addpassword component

---
+++ frontend/src/components/AddPassword.js
@@ -0,0 +1,10 @@
+function AddPassword(){
+    return(
+        <div className="shadow-md shadow-black flex flex-row h-[30vh] p-6
+                        mb-3">
+            Add Password
+        </div>
+    )
+}
+
+export default AddPassword;
