845c218e3293643b16995757d12a0cd42d1449f4
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-23 04:55:54 -0400

    added pwdcontainer component and addpassword component

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import Sidebar from "./Sidebar";
-
+import Passwords from "./Passwords";
+import AddPassword from "./AddPassword";
 
 function Dashboard(){
     return(
         <div className="w-full h-screen text-3xl text-gray-300 flex flex-row">
             <Sidebar />
-           
+            <div className=" max-w-screen">
+                <AddPassword/>
+                <Passwords />
+            </div>
         </div>
     )
 }
