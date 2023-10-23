6fbcb5c7fa7e2a013337ceea86aaefb0d7430969
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-22 20:06:24 -0400

    added route for dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Login from "./components/Login.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/Register.js";
+import Dashboard from "./components/Dashboard.js";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" Component={Login}/>
         <Route path="/register" Component={Register}/>
+        <Route path="/home" Component={Dashboard}/>
       </Routes>
     </BrowserRouter>
   );
