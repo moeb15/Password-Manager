4d26d2e7f7af4b1b02200084380c1d437dba2fbf
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-23 23:38:22 -0400

    added handleCreate function

--- frontend/src/components/AddPassword.js
+++ frontend/src/components/AddPassword.js
@@ -1,7 +1,43 @@
 import { AiFillAppstore,AiFillLock,AiFillFileAdd } from "react-icons/ai";
 import { BsFillKeyFill } from "react-icons/bs";
+import { useState } from "react";
 
 function AddPassword(){
+    const [ app,setApp ] = useState("");
+    const [ pwd,setPwd ] = useState("");
+    const [ key,setKey ] = useState("");
+
+    const handleCreate = async(e) => {
+        e.preventDefault();
+        const add_url = "http://localhost:8080/api/pwd"
+        try{
+            const req = {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                    Authorization:`Bearer ${localStorage.getItem("access_token")}`,
+                    "Refresh":localStorage.getItem("refresh_token")
+                },
+                body:JSON.stringify({
+                    application:app,
+                    password:pwd,
+                    key:key
+                })
+            }
+
+            const response = await fetch(add_url,req)
+            if(response.status === 201){
+                alert("Password saved")
+                const json = await response.json()
+                if(json.updated_token !== ""){
+                    localStorage.setItem("access_token",json.updated_token)
+                }
+            }
+        }catch (error){
+            alert(error);
+        }
+    }
+
     return(
         <div className="shadow-md shadow-black flex flex-row h-[20vh] p-6
                         mb-3">
@@ -14,6 +50,8 @@ function AddPassword(){
                     <AiFillAppstore size={30} className="mx-3"/>
                     <input type="text" 
                         placeholder="Application Name"
+                        value={app}
+                        onChange={e=>{setApp(e.target.value)}}
                         className="text-sm w-full text-gray-330 focus:outline-none
                                     bg-transparent ml-2"/>
                 </div>
@@ -24,6 +62,8 @@ function AddPassword(){
                     <AiFillLock size={30} className="mx-3"/>
                     <input type="password" 
                         placeholder="Password"
+                        value={pwd}
+                        onChange={e=>{setPwd(e.target.value)}}
                         className="text-sm w-full text-gray-330 focus:outline-none
                                     bg-transparent ml-2"/>
                 </div>
@@ -34,11 +74,14 @@ function AddPassword(){
                     <BsFillKeyFill size={30} className="mx-3"/>
                     <input type="password" 
                         placeholder="Encryption Key"
+                        value={key}
+                        onChange={e=>{setKey(e.target.value)}}
                         className="text-sm w-full text-gray-330 focus:outline-none
                                     bg-transparent ml-2"/>
                 </div>
             </div>
-            <AiFillFileAdd size={30} className="ml-[15vh] cursor-pointer"/>
+            <AiFillFileAdd size={30} className="ml-[15vh] cursor-pointer"
+                           onClick={handleCreate}/>
             </div>
         </div>
     )
