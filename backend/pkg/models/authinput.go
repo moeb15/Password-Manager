8cca1b58fa9ea9c7c71f9e177c7719032e32d782
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-24 09:36:51 -0400

    added email to users model

--- backend/pkg/models/authinput.go
+++ backend/pkg/models/authinput.go
@@ -1,7 +1,7 @@
 package models
 
 type AuthInput struct {
-	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -3,6 +3,7 @@ package models
 type User struct {
 	ID        string `json:"id" bson:"_id"`
 	Username  string `json:"username"`
+	Email     string `json:"email"`
 	Password  string `json:"password"`
 	MasterKey string `json:"masterkey"`
 }
