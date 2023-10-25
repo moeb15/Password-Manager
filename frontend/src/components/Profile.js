b341b12af5043bdf90ec3d8a33a51abf47fd3153
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-25 03:57:15 -0400

    scales for mobile screens

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -40,14 +40,14 @@ function Profile(){
     }
 
     return(
-        <div className="w-screen h-screen flex flex-col mt-5 sm:mt-3">
-            <div className="w-full h-[21vh] flex flex-col shadow-md shadow-black p-5">
+        <div className="w-screen h-screen flex flex-col mt-5 sm:mt-3 ">
+            <div className="w-full h-[30vh] sm:h-[21vh] flex flex-col shadow-md shadow-black p-5">
                 <h2 className="underline decoration-pink-600 my-2">Delete Account</h2>
-                <div className="bg-[#2a254e] w-fit h-full flex flex-col rounded-md
+                <div className="bg-[#2a254e] w-full sm:w-fit h-full flex flex-col rounded-md
                                 sm:grid sm:grid-cols-[50vh,50vh,10vh] items-center justify-center">
 
                     <div className="flex rounded-md items-center bg-slate-700 
-                                h-[5vh] text-left w-[30vh] sm:w-[34vh] lg:w-[35vh] mx-3">
+                                h-[5vh] text-left w-[30vh] sm:w-[34vh] lg:w-[35vh] mx-3 my-2 sm:my-0">
                         <MdEmail size={30} className="mx-3"/>
                         <input type="text"
                                placeholder="Email"
@@ -58,7 +58,7 @@ function Profile(){
                     </div>
 
                     <div className="flex rounded-md items-center bg-slate-700 
-                                h-[5vh] text-left w-[30vh] sm:w-[34vh] lg:w-[35vh] mx-3">
+                                h-[5vh] text-left w-[30vh] sm:w-[34vh] lg:w-[35vh] mx-3 my-2 sm:my-0">
                         <AiFillLock size={30} className="mx-3"/>
                         <input type="password"
                                placeholder="Password"
