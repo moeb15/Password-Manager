fb4eb401ba5bfd87e9a0752dda20f1010ee55380
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-24 22:53:48 -0400

    added username field

--- frontend/src/components/AddPassword.js
+++ frontend/src/components/AddPassword.js
@@ -1,9 +1,11 @@
 import { AiFillAppstore,AiFillLock,AiFillFileAdd } from "react-icons/ai";
 import { BsFillKeyFill } from "react-icons/bs";
 import { useState } from "react";
+import { BiSolidUser } from "react-icons/bi";
 
 function AddPassword(){
     const [ app,setApp ] = useState("");
+    const [ user,setUser ] = useState("");
     const [ pwd,setPwd ] = useState("");
     const [ key,setKey ] = useState("");
 
@@ -21,7 +23,8 @@ function AddPassword(){
                 body:JSON.stringify({
                     application:app,
                     password:pwd,
-                    key:key
+                    key:key,
+                    username:user
                 })
             }
 
@@ -40,14 +43,14 @@ function AddPassword(){
     }
 
     return(
-        <div className="shadow-md shadow-black flex flex-row h-fit lg:h-[20vh] p-6
+        <div className="shadow-md shadow-black flex flex-row h-fit lg:h-[21vh] p-6
                         mb-3 w-full justify-center sm:justify-normal">
             <div className="bg-[#2a254e] text-lg text-gray-300 w-fit
-                        h-full flex flex-col lg:grid lg:grid-cols-4 items-center p-3
-                        rounded-md mt-3">
+                        h-full flex flex-col lg:grid lg:grid-cols-[32vh,32vh,32vh,32vh,10vh] 
+                        items-center p-3 rounded-md mt-3 gap-x-12">
             <div className="h-[7vh] sm:mx-3 mt-3 lg:mt-0 ">
                 <div className="flex flex-row rounded-md items-center
-                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[40vh]">
+                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[35vh]">
                     <AiFillAppstore size={30} className="mx-3"/>
                     <input type="text" 
                         placeholder="Application Name"
@@ -59,7 +62,20 @@ function AddPassword(){
             </div>
             <div className="h-[7vh] sm:mx-3 mt-3 lg:mt-0">
                 <div className="flex rounded-md items-center
-                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[40vh]">
+                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[35vh]">
+                    <BiSolidUser size={30} className="mx-3"/>
+                    <input type="text" 
+                        placeholder="Username"
+                        value={user}
+                        onChange={e=>{setUser(e.target.value)}}
+                        className="text-sm w-full text-gray-330 focus:outline-none
+                                    bg-transparent ml-2"/>
+                </div>
+            </div>
+
+            <div className="h-[7vh] sm:mx-3 mt-3 lg:mt-0">
+                <div className="flex rounded-md items-center
+                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[35vh]">
                     <AiFillLock size={30} className="mx-3"/>
                     <input type="password" 
                         placeholder="Password"
@@ -69,9 +85,10 @@ function AddPassword(){
                                     bg-transparent ml-2"/>
                 </div>
             </div>
+
             <div className="h-[7vh] sm:mx-4 mt-3 lg:mt-0">
                 <div className="flex rounded-md items-center
-                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[40vh]">
+                bg-slate-700 h-full text-left w-[30vh] sm:w-[34vh] lg:w-[35vh]">
                     <BsFillKeyFill size={30} className="mx-3"/>
                     <input type="password" 
                         placeholder="Encryption Key"
@@ -81,7 +98,7 @@ function AddPassword(){
                                     bg-transparent ml-2"/>
                 </div>
             </div>
-            <AiFillFileAdd size={30} className="lg:ml-[34vh] cursor-pointer mt-3 lg:mt-0"
+            <AiFillFileAdd size={30} className=" cursor-pointer mt-3 lg:m-3"
                            onClick={handleCreate}/>
             </div>
         </div>
--- frontend/src/components/PwdContainer.js
+++ frontend/src/components/PwdContainer.js
@@ -1,12 +1,17 @@
-import { AiFillAppstore,AiFillLock,AiFillDelete,AiFillEdit } from "react-icons/ai"
-import { BsFillKeyFill } from "react-icons/bs";
+import { AiFillAppstore,AiFillDelete,AiFillEdit,AiFillLock } from "react-icons/ai"
+import { BsFillKeyFill,BsSaveFill } from "react-icons/bs";
+import { BiSolidUser } from "react-icons/bi";
 import { IoCopySharp } from "react-icons/io5";
 import { useState } from "react";
 
 function PwdContainer({props}){
     const [ key,setKey ] = useState("");
+    const [ new_pwd,setNewPwd ] = useState("");
     const [ pwdData,setPwdData ] = useState({});
+    const [ edit,setEdit ] = useState(false);
 
+    const handleEdit = () => setEdit(!edit);
+ 
     const handleCopy = async(e) => {
         e.preventDefault();
         const fetchpwd_url = `${process.env.REACT_APP_API_URL}/pwd/decrypt`
@@ -19,6 +24,7 @@ function PwdContainer({props}){
             },
             body:JSON.stringify({
                 application:props.application,
+                username:props.username,
                 key:key,
             })
         }
@@ -62,20 +68,69 @@ function PwdContainer({props}){
         }
     }
 
+    const submitEdit = async(e) => {
+        setEdit(!edit)
+        e.preventDefault();
+        const del_url = `${process.env.REACT_APP_API_URL}/pwd`
+        try{
+            const req = {
+                method:"PATCH",
+                headers:{
+                    "Content-Type":"application/json",
+                    Authorization:`Bearer ${localStorage.getItem("access_token")}`,
+                    Refresh:localStorage.getItem("refresh_token")
+                },
+                body:JSON.stringify({
+                    application:props.application,
+                    username:props.username,
+                    password:new_pwd,
+                    key:key
+                })
+            }
+            const response = await fetch(del_url,req)
+            if(response.status === 200){
+                const json = await response.json()
+                if (json.updated_token !== ""){
+                    localStorage.setItem("access_token",json.updated_token)
+                }
+                alert("Password updated")
+            }
+        }catch(error){
+            alert(error);
+        }
+    }
+
     return(
         <div className="bg-[#2a254e] text-lg text-gray-300 sm:w-fit
                         h-fit flex flex-col md:flex-row items-center p-3
                         rounded-md mt-3">
             <div className="flex flex-col items-center w-fit
-                            lg:grid lg:grid-cols-3 ">
+                            lg:grid lg:grid-cols-[34vh,30vh,40vh,60vh]">
                 <div className="flex flex-row">
                     <AiFillAppstore size={25} className="mx-3"/>
                     <h3>{props.application}</h3>
                 </div> 
-                <div className="hidden lg:flex flex-row mx-[-32vh]">
-                    <AiFillLock size={25} className="mx-3"/>
-                    <h3>{props.password}</h3>
+                <div className="flex flex-row my-3 sm:my-0">
+                    <BiSolidUser size={25} className="mx-3"/>
+                    <h3>{props.username}</h3>
                 </div>
+
+                <div className="flex flex-col items-center sm:flex-row sm:my-0 my-3">
+                    <div className="h-full pl-[1vh] lg:pl-[10vh]">
+                        <div className="flex rounded-md items-center
+                        bg-slate-700 h-full text-left sm:w-[30vh]">
+                            <AiFillLock size={25} className="mx-3"/>
+                            <input type="password" 
+                                placeholder="New Password"
+                                value={new_pwd}
+                                onChange={e=>{setNewPwd(e.target.value)}}
+                                className="text-sm w-full text-gray-330 focus:outline-none
+                                            bg-transparent ml-2"
+                                readOnly={!edit}/>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="flex flex-col items-center sm:flex-row ">
                     <div className="h-full pl-[1vh] lg:pl-[10vh]">
                         <div className="flex rounded-md items-center
@@ -89,13 +144,22 @@ function PwdContainer({props}){
                                             bg-transparent ml-2"/>
                         </div>
                     </div>
+                    {!edit ? 
                     <div className="flex flex-row mt-5 sm:mt-0">
                         <IoCopySharp size={25} className="ml-5 cursor-pointer"
                                     onClick={handleCopy}/>
                         <AiFillDelete size={30} className="ml-5 cursor-pointer"
                                     onClick={handleDel}/>
-                        <AiFillEdit size={30} className="ml-4 cursor-pointer"/>
+                        <AiFillEdit size={30} className="ml-4 cursor-pointer"
+                                    onClick={handleEdit}/>
                     </div>
+                    :
+                    <div className="flex flex-row mt-5 sm:mt-0">
+                        <BsSaveFill size={30} className="ml-[9vh] cursor-pointer"
+                                    onClick={submitEdit}/>
+                        <AiFillEdit size={30} className="ml-[2vh] cursor-pointer"
+                                    onClick={handleEdit}/>
+                    </div>}
                 </div>
             </div>
         </div>
