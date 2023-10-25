e00ff18dba30f7a66be4794ba9331bb06a91dd0e
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-25 03:56:07 -0400

    added deletionemail function

--- backend/internal/auth/email.go
+++ backend/internal/auth/email.go
@@ -21,3 +21,17 @@ func RegistrationEmail(user models.User) error {
 	err := dialer.DialAndSend(msg)
 	return err
 }
+
+func DeletionEmail(user models.User) error {
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", os.Getenv("OWNER_EMAIL"))
+	msg.SetHeader("To", user.Email)
+	msg.SetHeader("Subject", "Account deleted")
+	msg.SetBody("text/html", fmt.Sprintf("Account deletion successful <b>%s</b>", user.Username))
+
+	dialer := gomail.NewDialer("smtp.gmail.com", 587, os.Getenv("OWNER_EMAIL"),
+		os.Getenv("OWNER_PWD"))
+
+	err := dialer.DialAndSend(msg)
+	return err
+}
