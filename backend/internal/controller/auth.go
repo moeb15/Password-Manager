0ca964d304fe81e0417956abcda25fa54e5b0167
Mujtaba Bader <ma2bader@uwaterloo.ca>
2023-10-26 05:24:56 -0400

    added handler for updating account password

--- backend/internal/controller/auth.go
+++ backend/internal/controller/auth.go
@@ -37,3 +37,29 @@ func DeleteAccount(c *gin.Context) {
 
 	c.JSON(http.StatusNotFound, gin.H{"data": ""})
 }
+
+func UpdateAccount(c *gin.Context) {
+	db := c.MustGet("db_conn").(*database.DB)
+	var pwd_update models.AuthUserPwd
+	if err := c.ShouldBindJSON(&pwd_update); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := auth.CurrentUser(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+	if !helpers.CompareHashes(pwd_update.OldPassword, user.Password) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials"})
+		return
+	}
+	err = db.UpdateAccount(user, pwd_update.NewPassword)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	auth.AccountUpdated(user)
+
+	c.JSON(http.StatusOK, gin.H{"data": "", "updated_token": c.Request.Header.Get("Updated_Token")})
+}
